36a79d44f1417deb26232321388cd82ebd815d1a
svsairevanth12 <157276603+svsairevanth12@users.noreply.github.com>
2024-12-29 01:15:39 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="mb-3 mt-5 flex h-16 w-full flex-col items-center justify-between space-y-3 px-3 pt-4 text-center sm:mb-0 sm:h-20 sm:flex-row sm:pt-2">
       <div className="flex space-x-4 pb-4 sm:pb-0">
         <Link
-          href="https://twitter.com/osanseviero"
+          href="https://x.com/sai_revanth_12"
           className="group"
           aria-label="Twitter"
           target="_blank"
